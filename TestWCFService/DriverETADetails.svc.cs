10d6a29a9f1485a1bb3bceda70ac465b3549c100
shyam kumar <kumarshyam5@gmail.com>
2016-03-14 01:13:21 -0500

    First Commit

---
+++ TestWCFService/DriverETADetails.svc.cs
@@ -0,0 +1,73 @@
+﻿#region namespace
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using TestWCFService.BusinessObjects;
+#endregion
+
+namespace TestWCFService
+{
+    /// <summary>
+    /// class for handling driver details
+    /// </summary>
+    [ServiceBehavior(AddressFilterMode = AddressFilterMode.Any)]
+    public class DriverETADetails : IDriverETADetails
+    {
+
+        /// <summary>
+        /// Method to return driver details
+        /// </summary>
+        /// <param name="Id">driver id</param>
+        /// <returns>driver details</returns>
+        public DriverDetail getDriverDetails(string Id)
+        {
+           //creating a driverDetail object
+            DriverDetail details = new DriverDetail();
+            try
+            {
+                using (ETDDbModelContext context = new ETDDbModelContext())
+                {
+                    context.Configuration.ProxyCreationEnabled = false;
+                    int driverID = Convert.ToInt32(Id);
+                    //setting the values to details object
+                    details = (from c in context.DriverDetails
+                               where c.driver_id == driverID
+                               select c).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            //returning the result
+            return details;
+        }
+
+        /// <summary>
+        /// Method to return driver details
+        /// </summary>
+        /// <returns>list of drivers</returns>
+        public List<DriverDetail> getDrivers()
+        {
+            //creating a list of drivers
+            List<DriverDetail> details = new List<DriverDetail>();
+            try
+            {
+                using (ETDDbModelContext context = new ETDDbModelContext())
+                {
+                    context.Configuration.ProxyCreationEnabled = false;
+                    //setting the values to details
+                    details = (from c in context.DriverDetails
+                               select c).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            //returning the result
+            return details;
+        }
+    }
+}
---
+++ TestWCFService/IDriverETADetails.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+#region Namespaces
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using TestWCFService.BusinessObjects;
+#endregion
+
+namespace TestWCFService
+{
+    
+    /// <summary>
+    /// Interface for DriverETA details
+    /// </summary>
+    [ServiceContract]
+    public interface IDriverETADetails
+    {
+        /// <summary>
+        /// Method to return DriverDetails
+        /// </summary>
+        /// <param name="Id">driver id</param>
+        /// <returns>driver details</returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "driver/{id}")]
+        DriverDetail getDriverDetails(string Id);
+
+
+        /// <summary>
+        /// Method to return drivers
+        /// </summary>
+        /// <returns>list of drivers</returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "/drivers")]
+        List<DriverDetail> getDrivers();
+
+        
+    }
+}
